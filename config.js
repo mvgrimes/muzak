5462d8e37bf1becf41c0f5216d00d4d97f3e14e5
Mark Grimes <mgrimes@cpan.org>
2017-07-04 11:24:24 -0400

    Adds more playlists

--- config.js
+++ config.js
@@ -21,7 +21,15 @@ config.playlists = {
   'dishwashing': 'pandora://1649044550041233992.mp3',
   'jack johnson': 'pandora://1451670186947013192.mp3',
   'james taylor': 'pandora://1247104798763805256.mp3',
-  'zac brown': 'pandora://1489272784537283144.mp3'
+  'zac brown': 'pandora://1489272784537283144.mp3',
+  'billy joel': 'pandora://2733584939730627144.mp3',
+  'tom petty': 'pandora://3611578032873040456.mp3',
+  'lyle lovett': 'pandora://44464503630359112.mp3',
+  'amos less': 'pandora://1712674494826441288.mp3',
+  'david grey': 'pandora://162868518528772680.mp3',
+  'the head and the heart': 'pandora://2789020817059506760.mp3',
+  'ray lamontagne': 'pandora://552400074928319048',
+  'robert earle keen': 'pandora://1232800826796304968.mp3'
 };
 
 module.exports = config;
