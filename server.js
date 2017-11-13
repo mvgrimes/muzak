f160c239c55c1c276e3a5f6eb873b5e3bf76c8c7
Mark Grimes <mgrimes@cpan.org>
2017-11-13 17:06:27 -0500

    Converts to locally hosted app

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const context = require('aws-lambda-mock-context');
+
+// lambda.js contains the lambda function for Alexa as in
+// https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs
+var lambda = require('./index');
+
+const SERVER_PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser.json({ type: 'application/json' }));
+
+// your service will be available
+app.post('/lms/', function(req, res) {
+  var ctx = context();
+  // TODO: try/catch lambda.handler
+  lambda.handler(req.body, ctx);
+  ctx.Promise
+    .then(resp => {
+      return res.status(200).json(resp);
+    })
+    .catch(err => {
+      console.log(err);
+    }); // TODO: better error handling
+});
+
+var httpsServer = http.createServer(app);
+
+httpsServer.listen(SERVER_PORT, function() {
+  console.log(`Alexa Skill service ready on: ${SERVER_PORT} via http`);
+});
+
